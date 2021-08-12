119087dbf0d23f84e2aaf9a1740736e6dfab421b
SophiaFan <fan.shou@northeastern.edu>
2021-08-12 16:30:34 -0700

    upload files

--- server.js
+++ server.js
@@ -34,13 +34,5 @@ app.use('/user', require('./routes/userRouter'));
 
 app.use("/api/products", productRoutes);
 
-
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'))
-  app.get('*', (req, res)=>{
-      res.sendFile(path.join(__dirname = 'client/build/index.html'))
-  })
-}
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
