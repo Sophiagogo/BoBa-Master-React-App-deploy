4d075e1c78ef9668c7abb5fbe26d56807c66edc9
SophiaFan <fan.shou@northeastern.edu>
2021-08-12 18:11:45 -0700

    heroku

--- server.js
+++ server.js
@@ -34,5 +34,17 @@ app.use('/user', require('./routes/userRouter'));
 
 app.use("/api/products", productRoutes);
 
+// Add for deploy purposes
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
