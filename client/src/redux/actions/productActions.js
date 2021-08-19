3ad557b84e5949b93f25df34f3ad1263759fc3ce
SophiaFan <fan.shou@northeastern.edu>
2021-08-18 20:43:53 -0700

    update route

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -5,7 +5,7 @@ export const getProducts = () => async (dispatch) => {
   try {
     dispatch({ type: actionTypes.GET_PRODUCTS_REQUEST });
 
-    const { data } = await axios.get("http://localhost:5000/api/products");
+    const { data } = await axios.get("/api/products");
 
     dispatch({
       type: actionTypes.GET_PRODUCTS_SUCCESS,
@@ -26,7 +26,7 @@ export const getProductDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_REQUEST });
 
-    const { data } = await axios.get(`http://localhost:5000/api/products/${id}`);
+    const { data } = await axios.get(`/api/products/${id}`);
 
     dispatch({
       type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS,
